7954d47c45a110791ed2c1169f17a8757f171ce7
Sherry Sun <sherry.sun@nxp.com>
2024-04-26 18:56:32 +0800

    MA-22420 [All platform] move all WiFi drvier code to repo root path
    Reason:for WCS's request and Marketing's suggestion, move wifi driver to
    root path
    Tested:on i.MX 8MQ
    
    Change the folder layout to make it more clear
    
    Change-Id: Idf5663adccc1dfe90d5b62e5ee31187bcae37dec
    Signed-off-by: Sherry Sun <sherry.sun@nxp.com>

--- mxm_wifiex/wlan_src/mapp/mlanconfig/Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH := $(my-dir)
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := mlanconfig
-OBJS = mlanconfig.c mlanhostcmd.c mlanmisc.c
-LOCAL_SRC_FILES := $(OBJS)
-LOCAL_MODULE_TAGS := optional
-
-include $(BUILD_EXECUTABLE)
